d3850c5cfbaeb0a3d32c0b622b0870915640bbb5
issaanjalee <68030079+issaanjalee@users.noreply.github.com>
2024-07-19 18:24:11 +0400

    main file

--- IOC_extract.py
+++ IOC_extract.py
@@ -9,13 +9,13 @@ from xlwt import Workbook
 
 from exchangelib import Credentials, Account
 # Input your credentials here
-email = 'testingforpy@outlook.com'
-password = 'testing@2023'
+email = 'your email'
+password = 'your password'
 # Connect to the Outlook account
 credentials = Credentials(email, password)
 account = Account(email, credentials=credentials, autodiscover=True)
 # Get the desired email by subject
-email_subject = 'Alock Ransomware'
+email_subject = ''
 email_items = account.inbox.filter(subject__contains=email_subject)
 if email_items:
     # Get the latest email with the specified subject
